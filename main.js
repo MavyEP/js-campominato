4da786fba8188372d4121a5296815bb78ccf67af
edoardo <mavyofficialep@gmail.com>
2020-04-28 00:12:10 +0200

    prgroess

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 //creo una variabile dove andranno inseriti i numeri generati random
 var numeri_scelti = [];
+var numeri_gia_inseriti = [];
 //generare le mine (16numeri casuali scelti dal computer da 1 a 100)
 var numero_mine_pc = generaNumero(1,100)
 console.log("I numeri scelti dal pc ovvero le mine sono: " + numero_mine_pc);
 
-//chiedere all'utente di scegliere il numero per la sua giocata
 
-//controllare se quel numero é uguale ad uno di quelli generati dal pc
-//se non é uguale a nessuno dei numeri del pc allora chiedere un altro numero e proseguire
-//se il numero é uguale ad uno di quelli del pc il gioco é finot : GameOver
-
-
+do {
+  var giocata_utente = parseInt(prompt("Fai la tua giocata! Inserisci un numero!"))
+  console.log("Il numero che hai scelto é: " + giocata_utente);
+  numeri_gia_inseriti.push(giocata_utente);
+  console.log("I numeri gia inseriti sono: " + numeri_gia_inseriti);
+} while (checkplay(giocata_utente , numero_mine_pc)!=false);
 
 
 //LA FUNZIONE PER GENERARE DEI NUMERI INTERI RANDOM
@@ -22,4 +23,16 @@ function generaNumero(min, max) {
     i++;
   }
   return numeri_scelti
+}
+
+//LA FUNZIONE PER VEDERE SE IL TIRO DELL'UTENTE VA BENE
+function checkplay(numero_di_controllo , numero_del_pc) {
+  for (var i = 0; i < numero_del_pc.length; i++) {
+      if (numero_del_pc[i] == numero_di_controllo) {
+        console.log("Il numero dell'utente: " + numero_di_controllo + " é uguale al numero che corrisponde ad una mina: " + numero_del_pc[i] + " !!!GAME OVER!!!");
+        return false;
+     } else {
+        console.log("Il numero é valido si continua a giocare!");
+      }
+  }
 }
